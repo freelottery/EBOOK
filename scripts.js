81fb294fde932228289283c054b02ed1743e4d80
freelottery <shanaltanmiyah@gmail.com>
2025-03-18 02:25:38 -0700

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("Website Loaded Successfully!");
+
+    document.querySelector(".buy-button").addEventListener("click", function () {
+        alert("Redirecting to payment gateway...");
+    });
+});
